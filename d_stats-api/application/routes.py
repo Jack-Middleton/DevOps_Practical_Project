22d368a7a604ccf8cfaa66872d219c926b5e073b
Jack <jackmiddleton96@hotmail.co.uk>
2022-03-24 10:50:20 +0000

    finished adding dependant stats
    and created a basic front-end template

--- d_stats-api/application/routes.py
+++ d_stats-api/application/routes.py
@@ -3,9 +3,16 @@ from flask import jsonify, request
 from random import randint
 
 
-@app.route('/dependant-stats', methods=['POST'])
-def dependant_stats():
 
+@app.route('/get-dependant-stats', methods=['POST'])
+def dependant_stats():
+    speed_cap = 20
+    speed_base = 1
+    physical_cap = 20
+    physical_base = 1
+    def_base = 1
+    str_base = 1
+    mid_base = 1
     # determine the jumping reach, which is dependant on height
     request_data = request.get_json()
     height = request_data['height']
@@ -51,7 +58,77 @@ def dependant_stats():
     # strength
     str_ = randint(physical_base, physical_cap)
 
+    # check if position is goalkeeper first, if so, determine GK stats
+    # otherwise, use position to determine caps for other stats
+    position = request_data['position']
 
+    # setting GK stats to False, to use as a boolean check later to determine which set of stats to return
+    GK_stats = False
+    if position == 'GK':
+        GK_stats = True
+        # Aerial Reach
+        aer = randint(1,20)
+        # Command of Area
+        coa = randint(1,20)
+        # Communication
+        comm = randint(1,20)
+        # Eccentricity
+        ecc = randint(1,20)
+        # First Touch
+        ft = randint(1,20)
+        # Handling
+        hand = randint(1,20)
+        # Kicking
+        kick = randint(1,20)
+        # One on Ones
+        one = randint(1,20)
+        # Punching (tendency)
+        punch = randint(1,20)
+        # Reflexes
+        ref = randint(1,20)
+        # Rushing Out (tendency)
+        rush = randint(1,20)
+        # Throwing
+        throw = randint(1,20)
+    elif 'B' in position:
+        # confirms that the player is one of the defensive positions
+        # will likely have a higher base tackling & marking
+        def_base = 9
+    elif 'M' in position:
+        # player is a midfielder, higher corners, crossing, long shots
+        # but they also will have decent forward abilities
+        mid_base = 9
+        str_base = 6
+    else: 
+        # player is a striker or winger, higher dribbling, finishing, but will also have decent corners and crossing
+        str_base = 9
+        mid_base = 6
 
+    # now that the caps are determined, another round of randomly generated stats
+    # corners
+    cor = randint(mid_base, 20)
+    # crossing
+    cross = randint(mid_base, 20)
+    # dribbling
+    drib = randint(str_base, 20)
+    # finishing
+    fin = randint(str_base, 20)
+    # heading 
+    head = randint(1,20)
+    # long shots
+    long = randint(mid_base, 20)
+    # long throws 
+    long_t = randint(1,20)
+    # marking
+    mark = randint(def_base, 20)
+    # tackling
+    tack = randint(def_base, 20)
 
-    return jsonify(jumping_reach=jumping, acceleration=acc, agility=agi, balance=bal, pace=pac, stamina=sta, strength=str_)
+    if GK_stats:
+        return jsonify(jumping_reach=jumping, acceleration=acc, agility=agi, balance=bal, pace=pac, stamina=sta, strength=str_, \
+        aerial_reach=aer, command_of_area=coa,communication=comm, eccentricity=ecc, first_touch=ft, handling=hand, kicking=kick, \
+        one_on_ones=one, punching_tendency=punch, reflexes=ref, rushing_out_tendency=rush, throwing=throw)
+    else:
+        return jsonify(jumping_reach=jumping, acceleration=acc, agility=agi, balance=bal, pace=pac, stamina=sta, strength=str_, \
+        corners=cor, crossing=cross, dribbling=drib, finishing=fin, heading=head, long_shots=long, long_throws=long_t, marking=mark, tackling=tack)
+
--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -1,11 +1,14 @@
 from application import app
 import requests
+from flask import render_template, url_for
 
-# nd_stats is shorthand for non dependant
+# nd_stats is shorthand for non dependant, much like d_stats is for dependant
 
 @app.route('/')
 def index():
     personal = requests.get('http://personal-api:5000/get-personal-info')
     nd_stats = requests.get('http://nd_stats-api:5000/get-nondependant-stats')
-    jumping = requests.post('http://d_stats-api:5000/dependant-stats', json=dict(height=personal.json()['height'], weight=['weight'], position= ['position']))
-    return f'{personal.json()} | {nd_stats.json()} | {jumping.json()}'
+    d_stats = requests.post('http://d_stats-api:5000/get-dependant-stats', json=dict(height=personal.json()['height'], weight=personal.json()['weight'],\
+         position= personal.json()['position']))
+    return render_template('index.html', personal_stats=personal.json(), non_dependant=nd_stats.json(), dependant=d_stats.json())
+
